107f40bf640f3fe5fc800e46f54356daf2cc64d0
Pm Pranta <heyiampm@gmail.com>
2026-03-12 22:50:07 +0600

    Code Change

--- smart alert system.ino
+++ smart alert system.ino
@@ -1,15 +1,12 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
 const int trigPin = 11;
 const int echoPin = 12;
 const int redLED = 8;
 const int yellowLED = 9;
 const int greenLED = 10;
 const int buzzer = 7;
-
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -28,8 +25,6 @@ void setup() {
 void loop() {
   long duration;
   int distance;
-
-  // আল্ট্রাসোনিক দিয়ে দূরত্ব মাপা
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -38,24 +33,18 @@ void loop() {
   
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
-
-  // দূরত্ব LCD তে দেখানো (ঐচ্ছিক কিন্তু সুবিধার জন্য)
   lcd.setCursor(0, 0);
   lcd.print("Dist: ");
   lcd.print(distance);
   lcd.print(" cm    ");
 
-  // লজিক চেক
   if (distance > 100 || distance <= 0) {
-    // ১০০ সেমির বেশি দূরে থাকলে নিরাপদ
     showSafe();
   } 
   else if (distance <= 100 && distance > 30) {
-    // ৩০ থেকে ১০০ সেমি এর মধ্যে সতর্কতা
     showWarning();
   } 
   else if (distance <= 30) {
-    // ৩০ সেমি এর কম হলে বিপদ
     showDanger();
   }
 
@@ -86,8 +75,6 @@ void showDanger() {
   digitalWrite(redLED, HIGH);
   lcd.setCursor(0, 1);
   lcd.print("DANGER!!        ");
-
-  // সাইরেন টোন
   for (int hz = 600; hz < 1100; hz++) {
     tone(buzzer, hz);
     delay(2);
